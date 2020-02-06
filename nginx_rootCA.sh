5941f80a4c5136613bc4bde527c4cd2d14a2bf1d
t33z3r <47032615+t33z3r@users.noreply.github.com>
2020-02-06 16:44:29 +0100

    Add files via upload

--- nginx_rootCA.sh
+++ nginx_rootCA.sh
@@ -8,9 +8,9 @@ cd /etc/pki/CA/private
 openssl genrsa -aes128 -out rootCA.key 2048
 openssl req -new -x509 -days 1825 -key /etc/pki/CA/private/rootCA.key -out /etc/pki/CA/certs/rootCA.crt
 yum install -y mod_ssl
-openssl genrsa -out /etc/pki/tls/private/nginx-selfsigned.key 2048
-openssl req -new -key /etc/pki/tls/private/nginx-selfsigned.key -out /etc/pki/tls/nginx-selfsigned.csr
-scp /etc/pki/tls/nginx-selfsigned.csr ~/nginx-selfsigned.csr
+openssl genrsa -out /etc/pki/tls/private/web-01.key 2048
+openssl req -new -key /etc/pki/tls/private/web-01.key -out /etc/pki/tls/web-01.csr
+scp /etc/pki/tls/web-01.csr ~/web-01.csr
 cd ~/
-openssl x509 -req -in nginx-selfsigned.csr -CA /etc/pki/CA/certs/rootCA.crt -CAkey /etc/pki/CA/private/rootCA.key -CAcreateserial -out nginx-selfsigned.crt -days 365
-scp ~/nginx-selfsigned.crt /etc/pki/tls/certs/nginx-selfsigned.crt
+openssl x509 -req -in web-01.csr -CA /etc/pki/CA/certs/rootCA.crt -CAkey /etc/pki/CA/private/rootCA.key -CAcreateserial -out web-01.crt -days 365
+scp ~/web-01.crt /etc/pki/tls/certs/web-01.crt
